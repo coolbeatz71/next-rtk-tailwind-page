1ea1a05bda4feab7ac3f0dd92b6d95eebe734dda
coolbeatz71 <sigmacool@gmail.com>
2025-08-19 19:10:26 +0200

    ci: auto-fix linting issue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,14 @@
-import Image from "next/image";
 import { Geist, Geist_Mono } from "next/font/google";
+import Image from "next/image";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
-    subsets: ["latin"],
+    subsets: ["latin"]
 });
 
 const geistMono = Geist_Mono({
     variable: "--font-geist-mono",
-    subsets: ["latin"],
+    subsets: ["latin"]
 });
 
 export default function Home() {
